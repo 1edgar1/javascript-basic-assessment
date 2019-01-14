615c7d7b833b78fef470d9da969694eb67161788
Andres <thimgana@gmail.com>
2019-01-14 16:06:57 -0700

    First Attempt

--- assessment.js
+++ assessment.js
@@ -1,10 +1,12 @@
 
 // #1 Create a variable called hello and assign it the string 'goodbye'
 
-
+var hello = 'goodbye'
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
 var iLove = 'I love';
+
+var iLoveCode = iLove + " code";
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -13,23 +15,40 @@ var iLove = 'I love';
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
 
+var bob = {
+	height: "6ft",
+	age: 24,
+	hair:{
+		style:"spikey",
+		color:"brown",
+	},
+presidentOfTheUnitedStates: false,
+likes:["apples","bananas","cherries"]
+}
 
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
 	type: 'polo',
 	color: 'red'
 };
+  myShirt.color="pink";
 
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
 	type: 'polo',
 	color: 'red'
 };
+myOtherShirt["type"]= "spandex";
 
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
+var zoo = {
+	"monkeys": 8,
+	"giraffes": 4,
+	"elephants":2,
+}
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -43,24 +62,28 @@ var snake = {
 
 //#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
 
+var rainbowColors = ["red","orange","yellow","green","blue", "indigo", "violet",];
+
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
-
+heroes.push("wonderwoman");
 // remove 'superman' and store him in a variable called firstHero
-
+var firstHero = heroes.shift();
 // add 'spongebob' to the start of the array
-
+heroes.unshift("spongebob");
 // remove 'flash' from the array and store him in a variable called secondHero
 
 // leave batman in the array but put a copy of him on a variable called thirdHero
-
+var thirdHero = heroes[1];
 
 
 
 // #9 Write a function called addItem that takes in an array and an item, adds the item to
 // the array, and returns the array with the added item.
+function addItem(item){
 
+}
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
 
